f75a793c36d79bdcebe1d079aa51b5848b2b7e7e
PabloBaronIT <pablob@pablobaron.com.ar>
2023-04-12 17:23:32 -0300

    Descubri Sacanta update

--- app/city/discover/page.jsx
+++ app/city/discover/page.jsx
@@ -10,46 +10,52 @@ export default function DiscoverSacanta() {
       {/* PUNTOS TURISTICOS */}
       <div className={styles.containerPoints}>
         <ul className={styles.turisticList}>
-          <li className={styles.turisticPoint}>
-            {/* <span className={styles.title}>Punto turistico N° 1</span> */}
-            <br />
-            <img
-              src="/banner-iglesia.jpg"
-              className={styles.imgTuristicPoint}
-            />
-            <br />
-          </li>
-          <p className={styles.spanDescription}>
-            Como en la mayoría de las localidades argentinas, en Sacanta una
-            Iglesia frente a la Plaza Principal es una de las edificaciones más
-            sobresalientes. La Iglesia San Miguel Arcángel fue construida
-            durante en la segunda década del siglo XX, por el Ingeniero Ignacio
-            Rodríguez y su ayudante Don Pinot Ferrero. El templo es una réplica
-            de una construcción de Piemonte, en Italia.
-          </p>
+          <div className={styles.containerItemList}>
+            <li className={styles.turisticPoint}>
+              {/* <span className={styles.title}>Punto turistico N° 1</span> */}
+              <br />
+              <img
+                src="/banner-iglesia.jpg"
+                className={styles.imgTuristicPoint}
+              />
+              <br />
+            </li>
+            <p className={styles.spanDescription}>
+              <h3>Descripción</h3>
+              Como en la mayoría de las localidades argentinas, en Sacanta una
+              Iglesia frente a la Plaza Principal es una de las edificaciones
+              más sobresalientes. La Iglesia San Miguel Arcángel fue construida
+              durante en la segunda década del siglo XX, por el Ingeniero
+              Ignacio Rodríguez y su ayudante Don Pinot Ferrero. El templo es
+              una réplica de una construcción de Piemonte, en Italia.
+            </p>
+          </div>
           <br />
           <br />
           <br />
           <br />
           <br />
-          <li className={styles.turisticPoint}>
-            {/* <span className={styles.title}>Punto turistico N° 1</span> */}
-            <br />
-            <img
-              src="/museo-sacanta.jpeg"
-              className={styles.imgTuristicPoint}
-            />
-            <br />
-          </li>
-          <p className={styles.spanDescription}>
-            Un lugar imperdible para sumergirse en el pasado que le dio origen a
-            la localidad de Sacanta es su Museo Regional. Reuniendo los más de
-            100 años de historia que fue escribiendo esta comunidad del este
-            cordobés, funciona en la conservada Ex - Estación del Ferrocarril.
-            Tanto el edificio como la colección de piezas que cobija fueron
-            declarados de Interés Municipal y componentes del Patrimonio
-            Arquitectónico de la ciudad en 2011.
-          </p>
+          <div className={styles.containerItemList}>
+            <li className={styles.turisticPoint}>
+              {/* <span className={styles.title}>Punto turistico N° 1</span> */}
+              <br />
+              <img
+                src="/museo-sacanta.jpeg"
+                className={styles.imgTuristicPoint}
+              />
+              <br />
+            </li>
+            <p className={styles.spanDescription}>
+              <h3>Descripción</h3>
+              Un lugar imperdible para sumergirse en el pasado que le dio origen
+              a la localidad de Sacanta es su Museo Regional. Reuniendo los más
+              de 100 años de historia que fue escribiendo esta comunidad del
+              este cordobés, funciona en la conservada Ex - Estación del
+              Ferrocarril. Tanto el edificio como la colección de piezas que
+              cobija fueron declarados de Interés Municipal y componentes del
+              Patrimonio Arquitectónico de la ciudad en 2011.
+            </p>
+          </div>
         </ul>
       </div>
     </>
